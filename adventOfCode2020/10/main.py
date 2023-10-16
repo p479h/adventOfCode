3c35e015506740bf1374b9ba1db5ea0d0fb603fd
Carvalho Ferreira, Pedro de <p.h.d.carvalho.ferreira@student.tue.nl>
2023-10-16 08:15:40 +0200

    Solved 10. Looked up how to do part2 properly.

--- adventOfCode2020/10/main.py
+++ adventOfCode2020/10/main.py
@@ -11,13 +11,20 @@ def load_data(path):
 
     return tuple(map(int, lines))
 
+def part2(data):
+    # Count number of states for each additional element
+    states = {data[0]:1}
+    for n in data[1:]: # -1 and -2 add states, -3 keeps old number of states
+        states[n] =  states.get(n-1,0)+states.get(n-2,0)+states.get(n-3,0) 
+    return states[data[-1]]
 
 def main():
     path = [test_data_path, real_data_path][1]
     data = load_data(path)
     sort = [0]+sorted(data)+[max(data)+3]
     diff = [i-j for i,j in zip(sort[1:], sort[:-1])]
-    print(Counter(diff))    
+    print(Counter(diff))
+    print(part2(sort))
 
 
 if __name__ == "__main__":
