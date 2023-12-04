c409467126932dfe59bd87605ca79af368fc537c
Pepe <phferreira479@gmail.com>
2023-12-04 14:02:09 +0100

    Solved day 4

--- adventOfCode2023/04/main.py
+++ adventOfCode2023/04/main.py
@@ -1,11 +1,18 @@
-def read(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
+import re 
+from collections import defaultdict
 
 def main():
-    test_path = "./test.txt"
-    data_path = "./data.txt"
-    data = read([test_path, data_path][0])
+    s = 0
+    matches = defaultdict(lambda: 1)
+    for li, line in enumerate(map(str.strip, open("./data.txt"))):
+        l1, l2 = tuple(map(lambda l: set(map(int,l.split())), re.findall(r"([\d ]{5,})", line)))
+        n_matches = len(l1&l2)
+        s += bool(n_matches)*2**(n_matches-1)
+        matches[li] += 0 # Ensure that this index exists
+        for i in range(1,n_matches+1):
+            matches[i+li] += matches[li]
+    print(s)
+    print(sum(matches.values()))
     return 0
 
 if __name__ == "__main__":
