8012270c15507c47749a3801b688ebb7ea625de1
Pepe <phferreira479@gmail.com>
2023-12-04 18:36:56 +0100

    Solved day 15

--- adventOfCode2020/15/main.py
+++ adventOfCode2020/15/main.py
@@ -1,5 +1,27 @@
-from pathlib import Path
+import matplotlib.pyplot as plt
 
-data_path = Path('./data')
-test_data_path = data_path/'test_data.txt'
-real_data_path = data_path/'data.txt'
+nums = tuple(map(int,"1,0,15,2,10,13".split(",")))
+# Generating first few turns 
+turns = [ # Turn, number, past_turn (-1 for first time )
+    (i, n, -1) for i,n in enumerate(nums)
+]
+last_spoken = {n: i for i,n in enumerate(nums)}
+spoken = set(nums[:-1])
+N =  30000000 
+for i in range(len(nums),N+1):
+    # Last spoken number 
+    n = turns[i-1][1]
+    # Finding out if number was spoken before last time(last said set to -1)
+    if (last_turn := turns[last_spoken[n]])[2] != -1: 
+        # Getting difference in turns
+        turn_before = turns[last_turn[2]]
+        n = last_turn[0] - turn_before[0]
+
+    else: # First time spoken
+        n = 0  
+
+    spoken.add(n) # Updating spoken numbers 
+    turns.append((i, n, last_spoken.get(n,-1)))
+    last_spoken[n] = i
+    
+print(turns[N-1])
