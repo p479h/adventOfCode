d9880401592400e2d8e127d52bbfc02d77117cdb
Pepe <phferreira479@gmail.com>
2023-12-09 15:08:44 +0100

    solved day 9

--- adventOfCode2023/09/main.py
+++ adventOfCode2023/09/main.py
@@ -1,11 +1,17 @@
-def read(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
+from functools import partial 
 
 def main():
-    test_path = "./test.txt"
-    data_path = "./data.txt"
-    data = read([test_path, data_path][0])
+    data = tuple(tuple(map(int, l.split())) for l in open("./data.txt"))
+    # Part 1 and 2
+    def f(part, l):
+        if not any(l): # All l are 0
+            return l[0]
+        if part == 1:
+            return l[-1] + f(part, [a-b for a,b in zip(l[1:], l[:-1])])
+        # Part 2
+        return l[0] - f(part, [a-b for a,b in zip(l[1:], l[:-1])])
+    print(sum(map(partial(f, 1), data))) # Part 1
+    print(sum(map(partial(f, 2), data))) # Part 2
     return 0
 
 if __name__ == "__main__":
