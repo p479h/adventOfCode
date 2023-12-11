7ca8152c45fc432194f76ad2652e56646a83583e
Pepe <phferreira479@gmail.com>
2023-12-11 13:45:39 +0100

    Simplified code

--- adventOfCode2022/06/6.py
+++ adventOfCode2022/06/6.py
@@ -1,8 +1,6 @@
 
-lines = (l.strip("\n") for l in open("input.txt", "r").readlines())
-
 char_per_signal = 14
-for line in lines:
+for line in open("input.txt", "r").read().splitlines():
     for i, char in enumerate(line, start = 3):
         sequence = set(line[i-char_per_signal+1:i+1])
         if len(sequence) == char_per_signal:
