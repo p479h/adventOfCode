e8d612dfe4f8edc5774d3799d88c2a72bbde1deb
Pepe <phferreira479@gmail.com>
2023-12-15 10:32:53 +0100

    Solved day 15

--- adventOfCode2023/15/main.py
+++ adventOfCode2023/15/main.py
@@ -1,11 +1,27 @@
-def read(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
+import re 
+from functools import reduce
 
 def main():
-    test_path = "./test.txt"
-    data_path = "./data.txt"
-    data = read([test_path, data_path][0])
+    sequence = open('data.txt').read()
+    
+    # Part 1
+    HASH = lambda s: reduce(lambda c, s: (c+ord(s))*17%256, s, 0)
+    print("Sum HASH: %i"%sum(map(HASH, sequence.split(","))))
+
+    # Part 2
+    boxes = [{} for _ in range(256)]
+    for step in re.findall(r"(\w+)([\-=])(\d*)", sequence):
+        i = HASH(step[0])
+        match step:
+            case h, "=", d:
+                boxes[i][h] = int(d)
+            case h, "-", _ if h in (box:=boxes[i]):
+                del box[h]
+
+    fp  = sum((i+1)*(j+1)*f
+              for i, b in enumerate(boxes)
+              for j,f in enumerate(b.values()))
+    print("Focusing power: %i"%fp)
     return 0
 
 if __name__ == "__main__":
