80397c3b9abf35361b686bee1e7bdfc5e62b6350
Pedro de Carvalho <phferreira479@gmail.com>
2024-07-07 22:55:05 +0200

    Imrpoved solution

--- adventOfCode2020/16/main.py
+++ adventOfCode2020/16/main.py
@@ -1,5 +1,20 @@
-from pathlib import Path
+import re 
+from itertools import chain
 
-data_path = Path('./data')
-test_data_path = data_path/'test_data.txt'
-real_data_path = data_path/'data.txt'
+fields, _, nearby = open("test.txt").read().split("\n\n")
+nearby = [tuple(map(int, l.split(","))) for l in nearby.splitlines()[1:]]
+ranges = {
+    re.search(r"\w+", l).group(): [range(int(a), int(b)+1) for a,b in re.findall(r"(\d+)-(\d+)", n)]
+    for l, n in (f.split(":") for f in fields.splitlines())
+}
+
+# Part 1
+print(sum((n for n in chain(*nearby) 
+                if not any(n in r for r in chain(*ranges.values())))))
+    
+# Part 2
+# Discarding wrong rows
+print(nearby)
+nearby = [l for l in nearby if all(any(n in r for r in chain(*ranges.values())) for n in l)] 
+print(nearby)
+                  
--- adventOfCode2023/22/main.py
+++ adventOfCode2023/22/main.py
@@ -1,11 +1,22 @@
-def read(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
+import re 
+import itertools as it
+
+# Info:
+# Maximum per column are 9,9,349 (no need to group them!)
+# Z coordinates in same block are sorted!
 
 def main():
-    test_path = "./test.txt"
-    data_path = "./data.txt"
-    data = read([test_path, data_path][0])
+    # loading the data     
+    bricks = [map(int,re.findall(r"\d+", l)) for l in open("data.txt").read().splitlines()]
+    # Formating the data 
+    bricks = sorted([list(map(list,zip(b[:3],b[3:]))) for b in map(list,bricks)],
+                    key = lambda c: c[2])
+    # 
+    layer = lambda b: dict((k,list(g)) for k,g in it.groupby(b,key=lambda b: b[-1][0]))
+    layers = layer(bricks)
+
+    
+    
     return 0
 
 if __name__ == "__main__":
